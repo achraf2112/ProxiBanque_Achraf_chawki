a0d52bba225eb6a5bacc96a728cee35e791db359
achraf2112 <chawki.achraf.contact@gmail.com>
2025-11-20 10:48:15 +0100

    initial commit

---
+++ src/main/java/org/example/proxibanque_achraf_chawki/ProxiBanqueAchrafChawkiApplication.java
@@ -0,0 +1,13 @@
+package org.example.proxibanque_achraf_chawki;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProxiBanqueAchrafChawkiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProxiBanqueAchrafChawkiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/proxibanque_achraf_chawki/ProxiBanqueAchrafChawkiApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.proxibanque_achraf_chawki;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProxiBanqueAchrafChawkiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
